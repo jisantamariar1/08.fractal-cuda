dc0e2d09fa5a6ca5ded80fbe7318928d5a8f288d
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-07-10 00:34:32 -0500

    correccion de errores

--- kernel.cu
+++ kernel.cu
@@ -1,88 +1,104 @@
 const int PALETTE_SIZE = 16;
-__constant__
+
+__constant__ //variable de solo lectura en la memoria de la GPU
 unsigned int color_ramp[PALETTE_SIZE];
 
-__device__
-uint32_t acotado(double x, double y, double cr, double ci, int max_iteraciones) {
+//****************************** */
+__device__ //funcion en la GPU, solo accesible desde la GPU
+uint32_t acotado(double x, double y, 
+    double c_r, double c_i, 
+    int max_iteraciones){
+    /*
+    dados: c, z0
+    Zn+1 = Zn^2 + c
+    */
+
     int iter = 1;
-    double zr = x; // Parte real de z.
-    double zi = y; // Parte imaginaria de z.
 
-    // OPTIMIZACIÓN: (zr*zr + zi*zi) < 4.0 es equivalente a abs(z) < 2.0.
-    // Se usa 4.0 porque así evitamos calcular la raíz cuadrada interna de abs().
-    while (iter < max_iteraciones && (zr * zr + zi * zi) < 4.0) {
-        
-        // Aplicamos binomio al cuadrado: (a + bi)^2 = (a^2 - b^2) + (2ab)i.
-        double dr = zr * zr - zi * zi + cr; // Nueva parte real.
-        double di = 2.0 * zr * zi + ci;     // Nueva parte imaginaria.
-        
-        zr = dr; // Actualizamos para la siguiente iteración.
+    double zr = x;
+    double zi = y;
+
+    while (iter < max_iteraciones && (zr*zr+zi*zi) < 4.0)
+    {
+        double dr = zr*zr-zi*zi+c_r;
+        double di = 2.0*zr*zi+c_i;
+        zr = dr;
         zi = di;
-        iter++;
-    }
 
-    if(iter < max_iteraciones){
-        int index = iter % PALETTE_SIZE; // Usamos el contador de iteraciones para elegir un color de la paleta.
-        return color_ramp[index]; // Devolvemos el color correspondiente de la paleta.
-        //return 0xFF0000FF; // Rojo si escapa.
+        iter ++;
+
     }
-    return 0xFF000000; // Negro si no escapa.
+    if(iter < max_iteraciones){
+        // nomras > 2
+        int index = iter % PALETTE_SIZE;
+        return color_ramp[index];
+    }
+    
+    // los bits esta alreves en cuanto a los colores
+    return 0xFF000000; // negro
+
 }
 
+//******************************* */
 
-__global__
+__global__ //funcion en la GPU
 void julia_kernel(
     double centro_real, double centro_imag,
     int num_iteraciones,
-    double x_min, double y_min, double x_max, double y_max, 
+    double x_min, double y_min, double x_max, double y_max,
     uint32_t width,
-     uint32_t height, 
-     uint32_t* pixel_buffer) {
-
-        double dx = (x_max - x_min) / width;
-        double dy = (y_max - y_min) / height;
-
-        int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-
-        if (index<width * height){
-            int i = index / width;
-            int j = index % width;
-
+    uint32_t heigt,
+    uint32_t *pixel_buffer)
+{
+    double dx = (x_max - x_min) / width;
+    double dy = (y_max - y_min) / heigt;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < width * heigt){
+          // z = x+yi = (x,y)
+          int i =index % width;
+          int j = index / width;
             double x = x_min + i * dx;
-            double y = y_min + j * dy;
+            double y = y_max - j * dy;
 
-            auto color = acotado(
-                x,y, centro_real, centro_imag, num_iteraciones); // Default color if max iterations reached
-            
-            pixel_buffer[j*width + i] = color;
-        }
+            //std::complex<double> z(x, y);
+
+            // similar al var
+            auto color = acotado(x,y, centro_real, centro_imag, num_iteraciones);
+
+            //index j*w + i
+            pixel_buffer[j * width + i] = color;
 
     }
+          
 
 
-    void copiar_paleta(unsigned int* h_palette) {
-        //copiar la paleta desde la cpu a la gpu
-        cudaMemcpyToSymbol(color_ramp, h_palette, PALETTE_SIZE * sizeof(unsigned int));
-    }
+
+}
+
+
+//---------------cpu
+void copiar_paleta(unsigned int *pallete_host)
+{
+    //forma de copiar variables del host a la gpu  cudaMemcpyToSymbol-- copiar la paleta desde la cpu a la gpu
+    cudaMemcpyToSymbol(color_ramp, pallete_host, sizeof(unsigned int) * PALETTE_SIZE);
+}
 
 
 void julia_gpu(
     double centro_real, double centro_imag,
     int num_iteraciones,
-    double x_min, double y_min, double x_max, double y_max, 
+    double x_min, double y_min, double x_max, double y_max,
     uint32_t width,
-     uint32_t height, 
-     uint32_t* pixel_buffer) {
-
-        int threads_per_block = 1024;
-        int blocks_per_grid = std::ceil((width * height)*1.0 / threads_per_block);
-
-        julia_kernel<<<blocks_per_grid, threads_per_block>>>(
-            centro_real, centro_imag,
-            num_iteraciones, 
-            x_min, y_min, x_max, y_max, 
-            width, height, 
-            pixel_buffer);
-
-     }
+    uint32_t heigt,
+    uint32_t *pixel_buffer)
+{
+    int threads_per_block = 1024;
+    int blocks_per_grid = std::ceil((width * heigt) * 1.0 / threads_per_block);
+    
+    julia_kernel<<<blocks_per_grid, threads_per_block>>>(
+        centro_real, centro_imag, 
+        num_iteraciones, 
+        x_min, y_min, x_max, y_max, 
+        width, heigt, 
+        pixel_buffer);
+}
--- main.cpp
+++ main.cpp
@@ -1,158 +1,156 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include <iostream>
-#include <complex>
-#include <cstdint>
 #include <fmt/core.h>
 #include <SFML/Graphics.hpp>
 #include <cuda_runtime.h>
-#ifdef _WIN32
-    #include <windows.h>
-#endif
+#include "palette.h"
 
-// Definimos la resolución de nuestra área de dibujo.
-#define WIDTH 1600
-#define HEIGHT 900
-std::complex<double> c(-0.7, 0.27015); // La semilla que da forma al fractal de Julia.
+#include <complex>
 
-// Definimos los límites del plano complejo donde queremos "mirar".
+// Pamarametro img
+#define ANCHO 1600
+#define ALTO 900
+
+// Parameteros
+int max_iteraciones = 10;
 double x_min = -1.5;
 double x_max = 1.5;
 double y_min = -1.0;
 double y_max = 1.0;
 
-int max_iteraciones = 10; // Parámetro dinámico: puede cambiar mientras el programa corre.
+// Complejo que almacena dobles
+std::complex<double> c(-0.71, 0.27015);
+uint32_t *host_pixel_buffer = nullptr;
+uint32_t *device_pixel_buffer = nullptr;
+extern void copiar_paleta(unsigned int *pallete_host);
 
-// buffer host
-uint32_t* host_pixel_buffer = nullptr;
-// buffer device
-uint32_t* device_pixel_buffer = nullptr;
+extern void julia_gpu(double centro_real, double centro_imag, int num_iteraciones, double x_min, double y_min, double x_max, double y_max, uint32_t width, uint32_t height, uint32_t *pixel_buffer);
 
-#define CHECK(expr) {                               \
-        auto internal_error = (expr);               \
-        if (internal_error!=cudaSuccess) {          \
-            fmt::println("{}: {} in {} at line {}", (int)internal_error, cudaGetErrorString(internal_error), __FILE__, __LINE__);    \
-            exit(EXIT_FAILURE);                     \
-        }                                           \
+#define CHECK(expr)                                                                                                               \
+    {                                                                                                                             \
+        auto internal_error = (expr);                                                                                             \
+        if (internal_error != cudaSuccess)                                                                                        \
+        {                                                                                                                         \
+            fmt::println("{}: {} in {} at line {}", (int)internal_error, cudaGetErrorString(internal_error), __FILE__, __LINE__); \
+            exit(EXIT_FAILURE);                                                                                                   \
+        }                                                                                                                         \
     }
 
-// Declaración externa de la función CUDA (Se añadió el ';' faltante)
-extern void julia_gpu(
-    double x_min, double y_min, double x_max, double y_max, 
-    uint32_t width, 
-    uint32_t height, 
-    uint32_t* pixel_buffer
-);
-
-int main() {
-    int deviceId = 0; // Seleccionamos el primer dispositivo CUDA disponible.
+int main()
+{
+    int deviceId = 0;
     cudaSetDevice(deviceId);
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, deviceId);
-
-    fmt::println("Device: {}", deviceProp.name);
+    fmt::print("Device Name: {}\n", deviceProp.name);
     fmt::println("Total memory: {} MB", deviceProp.totalGlobalMem / (1024 * 1024));
-
-    // 1. ASIGNACIÓN Y RESERVA DE MEMORIA
-    size_t buffer_size = WIDTH * HEIGHT * sizeof(uint32_t);
     
-    // Reservar memoria en el Host (CPU) y limpiarla a 0 (Color Negro ARGB/RGBA)
-    host_pixel_buffer = (uint32_t*)malloc(buffer_size);
+    
+    size_t buffer_size = ANCHO * ALTO * sizeof(uint32_t);
+    host_pixel_buffer = (uint32_t *)malloc(buffer_size);
+
     std::memset(host_pixel_buffer, 0, buffer_size);
-
-    // Reservar memoria en el Device (GPU)
     CHECK(cudaMalloc(&device_pixel_buffer, buffer_size));
+    copiar_paleta(color_ramp.data());
+    // graifica
 
-    // 2. INICIALIZAR UI Y MOTOR GRÁFICO (SFML)
-    sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGHT}), "Julia Set - CUDA SFML");
+    sf::RenderWindow window(sf::VideoMode({ANCHO, ALTO}), "Julia");
 
-    // Si es Windows, forzamos que la ventana se abra maximizada usando el handle nativo.
 #ifdef _WIN32
-    HWND hwnd = window.getNativeHandle(); 
-    ShowWindow(hwnd, SW_MAXIMIZE);        
+    HWND hwnd = window.getNativeHandle();
+    ShowWindow(hwnd, SW_MAXIMIZE); // Maximizar Ventana
 #endif
+    sf::Vector2u windowSize = {ANCHO, ALTO};
+    sf::Texture texture(windowSize);
+    sf::Sprite sprite(texture);
 
-    sf::Vector2u windowSize = {WIDTH, HEIGHT};
-    sf::Texture texture(windowSize); 
-    // El 'sprite' es el objeto que permite "dibujar" la textura en la ventana.
-    sf::Sprite sprite(texture); 
+    sf::Font font("arial.ttf");
+    sf::Text text(font, "Julia Set", 24);
+    text.setFillColor(sf::Color::White);
+    text.setPosition({10, 10});
+    text.setStyle(sf::Text::Bold);
 
-    // Cargamos una fuente y configuramos el texto que mostrará los FPS.
-    sf::Font font("arial.ttf"); 
-    sf::Text text(font, "Julia Set", 24); 
-    text.setFillColor(sf::Color::White); 
-    text.setPosition({10, 10}); 
-    text.setStyle(sf::Text::Bold); 
+    std::string options = "Up/Sown change iterations";
+    sf::Text textoptions(font, options, 20);
 
-    std::string options = "Up/Down: Change iterations";
-    sf::Text textOptions(font, options, 20);
-    textOptions.setFillColor(sf::Color::White);
-    textOptions.setStyle(sf::Text::Bold);
-    textOptions.setPosition({10, static_cast<float>(window.getSize().y - 40)}); // Posicionar en la parte inferior.
-
-    // Variables para medir el rendimiento (Frames Per Second).
+    textoptions.setStyle(sf::Text::Bold);
+    textoptions.setPosition({10, window.getView().getSize().y - 40});
+    // FPS
     int frames = 0;
     int fps = 0;
-    sf::Clock clock; 
+    sf::Clock clock;
 
-    std::string mode = "GPU CUDA (Inactivo)";
-
-    // 3. BUCLE PRINCIPAL (Game Loop): Se ejecuta hasta que se cierre la ventana.
     while (window.isOpen())
     {
-        // A. PROCESAR EVENTOS: Entrada del usuario.
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close(); // Cerrar el programa.
-            } else if(event->is<sf::Event::KeyReleased>()) {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            else if (event->is<sf::Event::KeyReleased>())
+            {
                 auto evt = event->getIf<sf::Event::KeyReleased>();
-                // Controlamos las iteraciones con las flechas del teclado.
-                switch(evt->scancode) {
-                    case sf::Keyboard::Scan::Up:
-                        max_iteraciones += 10; // Más detalle.
-                        break;
-                    case sf::Keyboard::Scan::Down:
-                        max_iteraciones -= 10; // Menos detalle.
-                        if(max_iteraciones < 10) max_iteraciones = 10;
-                        break;
-                    default:
-                        break;
+
+                switch (evt->scancode)
+                {
+                case sf::Keyboard::Scan::Up:
+                    max_iteraciones += 10;
+                    break;
+                case sf::Keyboard::Scan::Down:
+                    max_iteraciones -= 10;
+                    if (max_iteraciones < 10)
+                        max_iteraciones = 10;
+                    break;
                 }
             }
         }
+            std::string mode = "";
+            //---------------------------
+            // Dibuja Fractal dependera de la velocidad
+            mode = "GPU CUDA";
+            // dibujar en la gpu
+            // invocar kernel
+            // copiar la imagen gpu-cpu
+            julia_gpu(
+                c.real(), c.imag(), 
+                max_iteraciones, 
+                x_min, y_min, x_max, y_max,
+                 ANCHO, ALTO, 
+                 device_pixel_buffer);
+            CHECK(cudaGetLastError());
+            CHECK(cudaMemcpy(host_pixel_buffer, device_pixel_buffer, buffer_size, cudaMemcpyDeviceToHost));
+            //----------------------
 
-        // B. CÁLCULO DEL FRACTAL / LOGICA CUDA (Comentado de momento para mantener ventana en negro)
-        /*
-        julia_gpu(x_min, y_min, x_max, y_max, WIDTH, HEIGHT, device_pixel_buffer);
-        CHECK(cudaGetLastError()); // Corregido typo: cudaGeetLastError -> cudaGetLastError
-        CHECK(cudaMemcpy(host_pixel_buffer, device_pixel_buffer, buffer_size, cudaMemcpyDeviceToHost)); // Corregido typo: cudaMencpy
-        */
+            texture.update((const uint8_t *)host_pixel_buffer);
 
-        // C. ACTUALIZAR TEXTURA: 
-        // Como el buffer de la CPU está lleno de ceros por el memset inicial, la textura se actualizará en negro.
-        texture.update((const uint8_t*)host_pixel_buffer);
-        frames++;
+            frames++;
 
-        // D. CÁLCULO DE FPS: Cada vez que pase 1 segundo, actualizamos el contador.
-        if (clock.getElapsedTime().asSeconds() >= 1.0f){
-            fps = frames;
-            frames = 0;
-            clock.restart();
+            if (clock.getElapsedTime().asSeconds() >= 1.0f)
+            {
+                fps = frames;
+                frames = 0;
+                clock.restart();
+            }
+            auto msg = fmt::format("julia: iteraciones; {}.fps{}, Mode: {}", max_iteraciones, fps, mode);
+            text.setString(msg);
+
+            // Clear screen
+            window.clear();
+            {
+                window.draw(sprite);
+                window.draw(text);
+                window.draw(textoptions);
+            }
+
+            // Update the window
+            window.display();
         }
-
-        // E. ACTUALIZAR HUD: Formateamos el mensaje de texto.
-        auto msg = fmt::format("Julia Set: Iterations: {}, FPS: {}, Mode: {}", max_iteraciones, fps, mode);
-        text.setString(msg);
-
-        // F. RENDERIZADO:
-        window.clear(sf::Color::Black); // Limpiar la pantalla con fondo negro estricto.
-        window.draw(sprite);            // Dibujar el buffer texturizado (actualmente negro).
-        window.draw(text);              // Dibujar el contador de FPS encima.
-        window.draw(textOptions);       // Dibujar las opciones de control.
-        window.display();               // Intercambiar buffers para mostrar en pantalla.
+free(host_pixel_buffer);
+    cudaFree(device_pixel_buffer);
+      return 0;
     }
-
-    // 4. LIMPIEZA DE MEMORIA: Evita fugas de memoria al cerrar la app
-    free(host_pixel_buffer);
-    CHECK(cudaFree(device_pixel_buffer));
-
-    return 0;
-}
+      
